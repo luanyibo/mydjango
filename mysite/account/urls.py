327f9ac56cfd536203ddf457e573021a4429bc68
luanyibo <luanyibo_sky@163.com>
2018-02-28 13:57:20 +0800

    1.增加登录功能;2.美化界面

--- mysite/account/urls.py
+++ mysite/account/urls.py
@@ -1,4 +1,4 @@
-"""mysite URL Configuration
+"""account URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.0/topics/http/urls/
@@ -13,13 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
+
 from django.conf.urls import url, include
+from . import views
 
-
+app_name='account'
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    url('^blog/',include('blog.urls', namespace='blog')),
-    url('^account/', include('account.urls', namespace='account')),
+    url(r'^login/$', views.user_login, name='user_login'),
 ]
